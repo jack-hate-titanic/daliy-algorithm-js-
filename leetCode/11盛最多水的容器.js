5b384ea7a09e152493d9b24d4710bbf7f6f8e209
Y-wson <1002783067@qq.com>
2021-07-04 21:57:09 +0800

    feat:11盛最多水的容器

--- leetCode/11盛最多水的容器.js
+++ leetCode/11盛最多水的容器.js
@@ -1,18 +1,37 @@
-const maxArea = function (height) {
-  let area = [];
-  for (let i = 1; i <= height.length; i++) {
-    for (let j = 1; j <= height.length; j++) {
-      if (j - i > 0) {
-        area.push((j - i) * Math.min(height[i - 1], height[j - 1]));
-      }
-    }
-    area.push(i * height[i]);
-  }
-  console.log(area);
-  return Math.max(...area);
-};
-const height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
-const value = maxArea(height);
-console.log(value);
+// 暴力解法，时间复杂度非常高
+// const maxArea = function (height) {
+//   let area = [];
+//   for (let i = 1; i <= height.length; i++) {
+//     for (let j = 1; j <= height.length; j++) {
+//       if (j - i > 0) {
+//         area.push((j - i) * Math.min(height[i - 1], height[j - 1]));
+//       }
+//     }
+//     area.push(i * height[i]);
+//   }
+//   console.log(area);
+//   return Math.max(...area);
+// };
 
 // 使用双指针去思考
+
+const maxArea = function (height) {
+  // 定义两个指针
+  let first = 0;
+  let last = height.length - 1;
+  const result = [];
+  for (let i = 0; i < height.length; i++) {
+    if (height[last] >= height[first]) {
+      result.push((last - first) * height[first]);
+      first++;
+    } else {
+      result.push((last - first) * height[last]);
+      last--;
+    }
+  }
+  return Math.max(...result);
+};
+
+height = [2, 3, 4, 5, 18, 17, 6];
+const value = maxArea(height);
+console.log(value);
